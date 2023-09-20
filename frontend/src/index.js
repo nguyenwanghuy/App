d18f56405757f11d1845eecc489827ba3c9ff897
nguyenwanghuy <nguyenquanghuy04012001@gmail.com>
2023-09-20 17:31:13 +0700

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import {PostProvider} from './context/PostContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <PostProvider>
     <App />
     </PostProvider>
